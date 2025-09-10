15f5dcdd41da7a76cff67696c441be68e8793daf
Adonais Mario Bentancourt <abent06@jaguar.tamu.edu>
2025-09-10 02:00:50 +0000

    Started Lab 9/9/2025

--- Lab3_Adonais_Bentancourt.java
+++ Lab3_Adonais_Bentancourt.java
@@ -1,2 +1,21 @@
-// Write file header
+//[Adonais Bentancourt]
+// [September 9th 2025]
 
+import java.util.Scanner;
+
+public class Lab3_Adonais_Bentancourt
+{
+    public static void main(String[] args)
+    {
+        double INITIAL_VALUE_HOME, interestRate, FINAL_VALUE_HOME;
+        int NUMBER_ELAPSED_YEARS;
+
+        System.out.println("Please insert the initial value of the home ");
+        INITIA_VALUE_HOME = input.nextDouble();
+
+        System.out.println("Please insert the number of elapsed years: ");
+        NUMBER_ELAPSED_YEARS = input.nextDouble();
+    
+        
+    }
+}
